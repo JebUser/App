58022318360ea6fed2eb70d0889af37b071aee7a
Jebuser <juanestebanbecerragutierrez@gmail.com>
2025-05-26 22:44:07 -0500

    MOD: Se elimino el  subir por achivo, me da paja y estoy mamado

--- frontend/components/componentes_registro/inicio_registro.py
+++ frontend/components/componentes_registro/inicio_registro.py
@@ -3,22 +3,21 @@ from utils.utils import navigate_to
 
 def pantalla_inicio():
     st.markdown("## Registrar información")
+    st.markdown("### Registro proyectos u organizaciones")
+    st.write("Registrar un proyectos, actividades u organizaciones")
+    col1, col2, col3= st.columns(3, border=True)
 
-    col1, col2 = st.columns(2)
 
     with col1:
-        st.markdown("### Registro proyectos u organizaciones")
-        st.write("Registrar un proyecto o una organización mediante el ingreso de datos manualmente.")
-        if st.button("Registrar Proyecto"):
+        if st.button("Registrar proyecto"):
             navigate_to('registrar', 'registro_proyecto')
-        if st.button("Crear actividad"):
+        
+    with col2:
+        if st.button("Registrar actividad"):
             navigate_to('registrar', 'registro_evento')
+    
+    with col3:
         if st.button("Registrar organización"):
             navigate_to('registrar', 'registro_organizacion')
         
 
-    with col2:
-        st.markdown("### Registro por archivo")
-        st.write("Registrar organizaciones desde archivo EXCEL, PDF o imagen.")
-        if st.button("Subir archivo"):
-            navigate_to('registrar', 'registro_archivo')
